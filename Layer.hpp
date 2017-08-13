dabfa9a24fc83d9c2ea735711bab521f65085a11
edvjar01 <ed.jaras@arm.com>
2017-08-13 22:36:55 +0100

    Static layer running and main improvements.

--- Layer.hpp
+++ Layer.hpp
@@ -10,39 +10,16 @@
 
 #include <tuple>
 #include <time.h>
-#include <initializer_list>
-#include <vector>
 
-#define NEW_LAYER(L, v) Layer<CL ## L, NE ## L> v
+#define RUN_LAYER(L) Layer<CL ## L ## Layer, NE ## L ## Layer>::run
 
-/* Creates a layer with the specified type. 
- *
- * Abstracts running a CL and NEON layer using the same back-end data.
- */
+namespace ACLBench{
+
+/* Abstracts running a CL and NEON layer using the same back-end data. */
 template <class _L_CL, class _L_NE>
 class Layer
 {
 public:
-	/* Layer constructor. 
-	 *
-	 * Performs preliminary ACL layer set-up, by creating the Tensors.
-	 *
-	 * @param [in] i_[xyz] The dimensions of the image used.
-	 * @param [in] w_[xyz] The dimensions of the kernel/weights.
-	 *
-	 * TODO: This REALLY needs a rework. Currently this will work only for Convolution,
-	 * which is fine since that is what I am looking to benchmark,
-	 * HOWEVER, there is no point to this class and constructor anymore and run
-	 * should be made a static function. 
-	 *
-	 */
-	Layer(size_t i_x, size_t i_y, size_t i_z, size_t w_x, size_t w_y, size_t w_z)
-	:	m_i_shape(i_x, i_y, i_z)
-	,	m_w_shape(w_x, w_y, w_z)
-	{
-		CLScheduler::get().default_init();
-	}
-
 	/* Runs the layers.
 	 *
 	 * Runs the available layers for the specified amount of iterations, and returns
@@ -56,44 +33,45 @@ public:
 	 * @param [in]  info  The layer-specific information needed to configure the layer. Must be set-up by the user.
 	 */
 	template<typename _Info, class ... _TensorList>
-	void run(unsigned int iters, float *res, _Info&& info, _TensorList& ... ttl)
+	static void run(unsigned int iters, float *res, _Info&& info, _TensorList& ... ttl)
 	{
 		clock_t t;
+		_L_CL cl_layer;
+		_L_NE ne_layer;
+
+		/* Initialize the scheduler. */
+		CLScheduler::get().default_init();
 
 		/* Allocate the buffers and configure the layer.
 		 * We do this only once since in an actual use-case the size of the tensors
 		 * will be constant. */
-		configure<CLTensor, _L_CL>(info, ttl ...);
-		configure<Tensor, _L_NE>(info, ttl ...);
+		configure<CLTensor, _L_CL>(cl_layer, info, ttl ...);
+		configure<Tensor, _L_NE>(ne_layer, info, ttl ...);
 
 		for (size_t i = 0; i < iters; ++i)
 		{
 			/* Randomize the data once for both layers so nothing can be cached in any way. */
 			(ttl.randomize(), ...);
-			
+
 			/* Run NEON and write results. */
 			t = clock();
-			run_layer<Tensor, _L_NE>(fill<Tensor>(ttl) ...);
+			run_layer<Tensor, _L_NE>(ne_layer, fill<Tensor>(ttl) ...);
 			t = clock() - t;
 			res[i*2] = ((float)t)/CLOCKS_PER_SEC;
 
-			/* Run CL and write results. */	
+			/* Run CL and write results. */
 			t = clock();
-			run_layer<CLTensor, _L_CL>(fill<CLTensor>(ttl) ...);
+			run_layer<CLTensor, _L_CL>(cl_layer, fill<CLTensor>(ttl) ...);
 			t = clock() - t;
 			res[i*2+1] = ((float)t)/CLOCKS_PER_SEC;
 		}
 	}
 
-	/* Conversions. */
-	operator _L_CL&() { return m_cl_layer; }
-	operator _L_NE&() { return m_ne_layer; }
-
 private:
 
 	template<class _T, class _L, class ... _TensorList>
-	void run_layer(_TensorList& ... ttl)
-	{	
+	static void run_layer(_L& l, _TensorList& ... ttl)
+	{
 		/* Synchronize the data between CPU and GPU if we are using CL. */
 		if (std::is_same<_L, _L_CL>::value)
 		{
@@ -101,7 +79,7 @@ private:
 		}
 
 		/* Run the thing. */
-		((_L&)*this).run();
+		l.run();
 	}
 
 	/* Due to issues with how the Tensor and CLTensor classes are implemented
@@ -115,7 +93,7 @@ private:
 	 * as it will force the GPU mapping, which is a real bottleneck and the purpose
 	 * of this benchmark. */
 	template<class _T>
-	TestTensor& fill(TestTensor& tt)
+	static TestTensor& fill(TestTensor& tt)
 	{
 		tt.fill<_T>();
 		return tt;
@@ -125,7 +103,7 @@ private:
 	 * Should be done more nicely later.
 	 */
 	template<class _T>
-	TestTensor& ready(TestTensor& tt)
+	static TestTensor& ready(TestTensor& tt)
 	{
 		tt.set_ready<_T>();
 		return tt;
@@ -133,15 +111,12 @@ private:
 
 	/* Configures the layer with the given tensor list. */
 	template<class _Tensor, class _Layer, typename _Info, typename ... _TensorList>
-	void configure(_Info&& info, _TensorList& ... ttl)
+	static void configure(_Layer& l, _Info&& info, _TensorList& ... ttl)
 	{
-		static_cast<_Layer&>(*this).configure(&static_cast<_Tensor&>(ready<_Tensor>(ttl))..., info);
+		l.configure(&static_cast<_Tensor&>(ready<_Tensor>(ttl))..., info);
 
 	}
-
-	_L_CL m_cl_layer;
-	_L_NE m_ne_layer;
-
-	TensorShape m_i_shape, m_w_shape;
 };
+
+} /* namespace ACLBench */
 #endif /* LAYER_H */
--- main.cpp
+++ main.cpp
@@ -1,30 +1,70 @@
 #include "Layer.hpp"
 #include <stdio.h>
 
-#define ITERATIONS 20
+#define ITERATIONS 10000
 #define LAYERS 2 /* NEON & CL */
+
+using namespace ACLBench;
+
+/* Prints averages for each backend.
+ * If the last parameter is set to true, also prints data for each run, which
+ * can be used to write to file and then process the data later.
+ */
+void print_res(size_t sz, float *buff, bool raw = false)
+{
+	float count = sz/2.f;
+	float tot_cl = 0.f;
+	float tot_ne = 0.f;
+	float avg_cl = 0.f;
+	float avg_ne = 0.f;
+	for (size_t i = 0; i < sz; ++i)
+	{
+		if (raw)
+			printf("NEON: %f | CL: %f\n", buff[i*2], buff[i*2+1]);
+
+		tot_cl += buff[i*2+1];
+		tot_ne += buff[i*2];
+	}
+	avg_cl += tot_cl/count;
+	avg_ne += tot_ne/count;
+	if (raw)
+		printf("---");
+	printf("NEON: %f | CL: %f\n", avg_ne, avg_cl);
+
+}
 
 int main()
 {
-	/* Create the layer. */
-	NEW_LAYER(ConvolutionLayer, conv)(5, 5, 1, 5, 5, 8);
-	TestTensor in(TensorShape(5, 5, 1));
-	/* Weights. */
-	TestTensor weights(TensorShape(5, 5, 1, 8));
-	/* Bias. */
-	TestTensor bias(TensorShape(8));
-	/* Output image. */
-	TestTensor out(TensorShape(5, 5, 8));
-
-
 	/* Buffer needs to accomodate 1 floats per layer per iteration. */
 	float *buff = (float *)malloc(ITERATIONS * LAYERS * sizeof(float));
+
+	/* Procedure is:
+	 * -create tensors;
+	 * -RUN_LAYER;
+	 * -print.
+	 */
+
+	/*** CONVOLUTION ***/
+	TestTensor in(TensorShape(5, 5, 1));
+	TestTensor weights(TensorShape(5, 5, 1, 8));
+	TestTensor bias(TensorShape(8));
+	TestTensor out(TensorShape(5, 5, 8));
 	/* We need to pad our data by 2 to accommodate our kernel size. */
-	conv.run(ITERATIONS, buff, PadStrideInfo(1, 1, 2, 2), in, weights, bias, out);
-	/* Print the results. */
-	for (int i = 0; i < ITERATIONS; ++i)
-	{
-		printf("NEON: %f | CL: %f\n", buff[i*2], buff[i*2+1]);
-	}
+	RUN_LAYER(Convolution)(ITERATIONS, buff, PadStrideInfo(1, 1, 2, 2), in, weights, bias, out);
+	printf("===CONVOLUTION===\n");
+	print_res(ITERATIONS, buff);
+
+	/*** RELU ***/
+	TestTensor ac_in(TensorShape(20, 20, 8));
+	TestTensor ac_out(TensorShape(20, 20, 8));
+	RUN_LAYER(Activation)(ITERATIONS, buff,
+	                      ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU),
+	                      ac_in, ac_out);
+	printf("===RELU===\n");
+	print_res(ITERATIONS, buff);
+
+	/* Clean-up. */
+	free(buff);
+
 	return 0;
 }
